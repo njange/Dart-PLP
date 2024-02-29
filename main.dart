f215117625d6fb2bd3d51078c09da2d682ea936d
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-29 10:25:03 +0300

    strings

--- main.dart
+++ main.dart
@@ -1,6 +1,12 @@
-void main(){
-int num1 = 10;
-int num2 = 20;
-int sum = num1 + num2;
-print("The sum of $num1 and $num2 is $sum");
+void main() {
+  int num1 = 10;
+  int num2 = 20;
+  int sum = num1 + num2;
+  print("The sum of $num1 and $num2 is $sum");
+
+  String name = "John";
+  int age = 25;
+  double height = 6.2;
+
+  print("My name is $name. I am $age years old and $height feet tall");
 }
