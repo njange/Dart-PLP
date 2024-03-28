1f42ad81b7fd95f3cca2499f541d3fbb180311d7
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-28 06:46:40 +0300

    await,async,futures and fetching user data

---
+++ await_async_future.dart
@@ -0,0 +1,22 @@
+import "dart:async";
+
+void main() async {
+  // Start asynchronous operations
+  print("Start of main");
+  Future<String> fetchUserData() {
+    return Future.delayed(Duration(seconds: 3), () {
+      return "John Doe";
+    });
+  }
+
+  String result = await fetchUserData();
+
+  print("End of main - User data: $result");
+
+
+  Future<String> fetchUserDataRenamed() {
+    return Future.delayed(Duration(seconds: 3), () {
+      return "John Doe";
+    });
+  }
+}
