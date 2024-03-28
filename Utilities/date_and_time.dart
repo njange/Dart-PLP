487a4b98f74f27bb7c63b11a6cab7ac847db61f6
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-28 06:31:32 +0300

    utilities

---
+++ Utilities/date_and_time.dart
@@ -0,0 +1,9 @@
+void main(){
+  //get current date and time
+  DateTime now = DateTime.now();
+
+  //Calculate a future date by adding 7 days to the current date
+  DateTime futuredate = now.add(Duration(days: 7));
+
+  // Crea
+}
---
+++ date_and _time.dart
@@ -0,0 +1,6 @@
+void main(){
+  // Current date and time
+  DateTime now = DateTime.now();
+  print(now);
+  
+}
