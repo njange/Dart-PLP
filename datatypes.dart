c5ea7587f28e0b4fea66f8d0a73bf4e734334e92
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-04-11 14:57:21 +0300

    complete dashboard

--- datatypes.dart
+++ datatypes.dart
@@ -8,6 +8,13 @@ void main() {
   displayGameDashboard(gamedashboard);
 }
 void displayGameDashboard(List gamedashboard){
-  print("${gamedashboard[0][0] | gamedashboard[0][1] | gamedashboard[0][2]}");
+  print(
+    "${gamedashboard[0][0] | gamedashboard[0][1] | gamedashboard[0][2]}");
+  print("-----");
+  print(
+    "${gamedashboard[1][0] | gamedashboard[1][1] | gamedashboard[1][2]}");
+  print("-----");
+  print(
+    "${gamedashboard[2][0] | gamedashboard[2][1] | gamedashboard[2][2]}");
 }
 
